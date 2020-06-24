ac8636d0186ad2b56ef62342f879dcc2ef8be24e
DouglasHdezT <00004516@uca.edu.sv>
2020-06-24 15:28:13 -0600

    Fix: Exec

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ const { execFile } = require("child_process");
 const controller = {};
 
 controller.updateGet = (req, res) => {
-	execFile("sh",["init.sh"], (err, stdout, stderr) => {
+	execFile("sh",[`${process.env.PATHEXEC}/exec.sh`], (err, stdout, stderr) => {
 		if (err) return res.status(400).json({message: `Error executing file: ${stderr}`})
 
 		res.status(200).json({message: stdout});
