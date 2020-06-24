c5921b8b21aa18c735b77817d91a9f611d6f743a
DouglasHdezT <00004516@uca.edu.sv>
2020-06-24 15:08:33 -0600

    Add: Get exec support

--- controllers/index.js
+++ controllers/index.js
@@ -3,11 +3,15 @@ const { execFile } = require("child_process");
 const controller = {};
 
 controller.updateGet = (req, res) => {
-	res.status(200).json({});
+	execFile("../execFiles/exec.sh", (err, stdout, stderr) => {
+		if (err) return res.status(400).json({message: `Error executing file: ${stderr}`})
+
+		res.status(200).json({message: stdout});
+	})
 }
 
 controller.updatePost = (req, res) => {
-	console.log(req.body);
+	console.log(req.body.action);
 	
 	res.status(200).json({});
 }
